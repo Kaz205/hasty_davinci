98d4b87097ce0f009bd169d332d929e3f8b94eac
Kazuki Hashimoto <kaz205@tuta.io>
2020-05-16 18:54:58 +0900

    techpack: audio: Update tfa98xx driver
    Signed-off-by: Kazuki Hashimoto <kaz205@tuta.io>

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 SMP PREEMPT Sat May 16 15:26:08 JST 2020"
+#define LINUX_COMPILE_BY "kazuki"
+#define LINUX_COMPILE_HOST "kaz205"
+#define LINUX_COMPILER "Proton clang version 11.0.0 (https://github.com/llvm/llvm-project 73a9b7dee08ba74d75fffc93d0e8ebdf9aa8cce1) Found CUDA installation: /opt/cuda, version 10.2"
